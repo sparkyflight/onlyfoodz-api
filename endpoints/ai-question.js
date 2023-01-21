9dbb288ba76401f06caa109f85eee801aa7f0461
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-20 22:50:29 -0600

    Made it somewhat smart

--- endpoints/ai-question.js
+++ endpoints/ai-question.js
@@ -1,3 +1,6 @@
+const banana = require('@banana-dev/banana-dev');
+require("dotenv").config();
+
 module.exports = {
 	name: "ai/question",
 	method: "GET",
@@ -6,6 +9,18 @@ module.exports = {
 
            if (!question || question === "") return res.json({ error: "There was no question passed with request." });
            else {
-           }
-        }
+              const banana = require('@banana-dev/banana-dev');
+
+              const modelParameters = {
+                 "text": question,
+                 "length":250,
+                 "temperature": 0.9,
+                 "batchSize": 1
+              }
+
+              const output = await banana.run(process.env.BANANA_KEY, "gptj", modelParameters);
+              if (output) return res.json(output);
+              else return res.json({ error: "A unknown error occured while rendering your request" });
+          }
+       }
 };
