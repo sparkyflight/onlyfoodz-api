31c42b09021006eabe92853bb280cb0fc450da48
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-22 10:33:46 -0600

    Update recent_songs.js

--- endpoints/recent_songs.js
+++ endpoints/recent_songs.js
@@ -30,7 +30,7 @@ module.exports = {
                                             i["artistData"] = { error: err };
                                         });
 
-                                        console.log(i);
+                                        console.log(item);
                                         allData["items"].push(i);
                                     });
 
