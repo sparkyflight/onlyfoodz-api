36d28706a989ac9984619855c4909dc75f7cc8bf
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-16 11:21:56 -0600

    Update getuserwithtoken.js

--- endpoints/getuserwithtoken.js
+++ endpoints/getuserwithtoken.js
@@ -3,18 +3,7 @@ module.exports = {
 	method: "GET",
 	execute: async (req, res, database, Spotify) => {
 		const token = req.query.token;
-		const tokenInfo = await database.Tokens.get(token);
-
-		if (!tokenInfo)
-			res.status(404).send({
-				message:
-					"We couldn't fetch any information about this user in our database",
-				token: token,
-				error: true,
-				fatal: false,
-			});
-
-		const user = await database.Users.get({ UserID: tokenInfo.UserID });
+		const user = await database.Tokens.get(token);
 
 		if (user) res.send(user);
 		else
