bda1fa45b273fb85fc599bf0e8fb1f68b8ef0742
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-20 09:28:53 -0600

    Update list_users_posts.js

--- endpoints/list_users_posts.js
+++ endpoints/list_users_posts.js
@@ -8,32 +8,23 @@ module.exports = {
 
 		if (userid || type != "") {
 			if (type || type != "") {
-				switch (posts) {
-					/*
-                1 = Onlyfoodz
-                2 = Regular
-            */
+			/*
+                      1 = Onlyfoodz
+                      2 = Regular
+                   */
 
-					case "1":
-						posts = await database.Posts.getAllUserPosts(userid, 1);
-						break;
+			if (type === "1") posts = await database.Posts.getAllUserPosts(userid, 1);
+			else if (type === "2") posts = await database.Posts.getAllUserPosts(userid, 2);
+			else
+				posts = {
+					error: "The provided type is invalid",
+				};
 
-					case "2":
-						posts = await database.Posts.getAllUserPosts(userid, 2);
-						break;
-
-					default:
-						posts = {
-							error: "The provided type is invalid",
-						};
-						break;
-				}
-
-				return res.json(posts);
-			} else
-				res.status(404).json({
-					error: "There was no post type specified with the request.",
-				});
+			return res.json(posts);
+		} else
+			res.status(404).json({
+				error: "There was no post type specified with the request.",
+			});
 		} else
 			res.status(404).json({
 				error: "There was no user id specified with the request.",
