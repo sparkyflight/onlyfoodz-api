837993927c5af2792d45ed05fd8460d21c3596fb
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 19:29:16 -0600

    Create getuser.js

---
+++ endpoints/getuser.js
@@ -0,0 +1,18 @@
+module.exports = {
+	name: "users/get",
+	method: "GET",
+	execute: async (req, res, database, Spotify) => {
+		const userid = req.query.id;
+		const user = await database.Users.get({ UserID: userid });
+
+		if (user) res.send(user);
+		else
+			res.status(404).send({
+				message:
+					"We couldn't fetch any information about this user in our database",
+				token: token,
+				error: true,
+				fatal: false,
+			});
+	},
+};
