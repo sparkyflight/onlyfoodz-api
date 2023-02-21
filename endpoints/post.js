06e56814c6b462498b17a4331c414a2ba4248344
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-21 11:52:17 -0600

    Create post.js

---
+++ endpoints/post.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: "posts/post",
+	method: "POST",
+	execute: async (req, res, database, Spotify, cloudinary) => {
+           const data = req.body;
+           let response = {};
+
+           if (data["image"]) {
+              const image = cloudinary.uploader.upload(data["image"], {public_id: "posts"})
+
+              res.then((i) => {
+                 response["image_uri"] = i.secure_url;
+              }).catch(console.error);
+           } else response["image_uri"] = null;
+
+           response["caption"] = data["caption"];
+
+           return res.json(response);
+        }
+};
