0effa708af34a0958585583878d4c774c9445fb0
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-24 05:58:40 -0600

    Create spotify-stats.js

---
+++ endpoints/spotify-stats.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "spotify/statistics",
+	method: "GET",
+	execute: async (req, res, database, Spotify, cloudinary) => {
+            const topArtists = await Spotify.getMyTopArtists().then((res) => return data.body.items);
+            const topSongs = await Spotify.getMyTopSongs().then((res) => return data.body.items);
+
+            return res.json({
+                topArtists: topArtists,
+                topSongs: topSongs
+            });
+        }
+};
