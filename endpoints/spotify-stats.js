f2cf0e89e9a1def7d53ab3897a2e5efc5d074bab
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-24 05:59:30 -0600

    Update spotify-stats.js

--- endpoints/spotify-stats.js
+++ endpoints/spotify-stats.js
@@ -2,8 +2,8 @@ module.exports = {
 	name: "spotify/statistics",
 	method: "GET",
 	execute: async (req, res, database, Spotify, cloudinary) => {
-            const topArtists = await Spotify.getMyTopArtists().then((res) => return data.body.items);
-            const topSongs = await Spotify.getMyTopSongs().then((res) => return data.body.items);
+            const topArtists = await Spotify.getMyTopArtists().then((res) => { return data.body.items });
+            const topSongs = await Spotify.getMyTopSongs().then((res) => { return data.body.items });
 
             return res.json({
                 topArtists: topArtists,
