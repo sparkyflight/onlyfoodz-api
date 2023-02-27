93513c4fd787d09eec5b41cb26f405454fb32b8b
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-27 17:17:28 -0600

    Update getuser.js

--- endpoints/getuser.js
+++ endpoints/getuser.js
@@ -3,9 +3,14 @@ module.exports = {
 	method: "GET",
 	execute: async (req, res, database, Spotify, cloudinary) => {
 		const userid = req.query.id;
-		const user =
-			(await database.Users.get({ UserID: userid })) ||
-			(await database.Teams.get({ UserID: userid }));
+		let user = await database.Users.get({ UserID: userid });
+                
+                user["team"] = false;
+
+                if (!user || user.error) {
+                    user = await database.Teams.get({ UserID: userid });
+                    user["team"] = true;
+                }
 
 		if (user) res.send(user);
 		else
