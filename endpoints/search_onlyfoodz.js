16dfc590ca4c7cd3f9b7d1fe055d153c0c415786
selectdev <66577268+selectdev@users.noreply.github.com>
2023-05-12 14:49:06 -0500

    Update search_onlyfoodz.js

--- endpoints/search_onlyfoodz.js
+++ endpoints/search_onlyfoodz.js
@@ -1,1 +1,23 @@
+module.exports = { 
+         name: "onlyfoodz/search", 
+         method: "GET", 
+         execute: async (req, res, database, Spotify) => { 
+                 const query = req.query.query; 
 
+                 const users = await database.Users.find({
+                   Username: {
+                      $regex: query,
+                      $options: "i"
+                   }
+                 });
+  
+                 const posts = await database.Posts.find({
+                   Caption: {
+                      $regex: query,
+                      $options: "i"
+                   }
+                 });
+
+                 return res.json({ users, posts });
+         }, 
+ };
