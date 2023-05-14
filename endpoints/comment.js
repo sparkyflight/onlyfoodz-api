6b38b74bcd58ef37f408d9f2ed7f617ceefd1ec0
selectdev <66577268+selectdev@users.noreply.github.com>
2023-05-14 16:40:59 -0500

    Update comment.js

--- endpoints/comment.js
+++ endpoints/comment.js
@@ -1,7 +1,9 @@
 const crypto = require("node:crypto"); 
   
- module.exports = { 
+module.exports = { 
          name: "posts/comment", 
          method: "POST", 
-         execute: async (req, res, database, Spotify) => {}
- };
+         execute: async (req, res, database, Spotify) => {
+           const data = req.body;
+         }
+};
