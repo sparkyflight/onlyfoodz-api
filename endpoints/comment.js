1a3c8474a3b61b4bcbdb4cc3680255657bba1bf8
selectdev <66577268+selectdev@users.noreply.github.com>
2023-05-14 16:45:05 -0500

    Update comment.js

--- endpoints/comment.js
+++ endpoints/comment.js
@@ -4,6 +4,17 @@ module.exports = {
          name: "posts/comment", 
          method: "POST", 
          execute: async (req, res, database, Spotify) => {
-           const data = req.body;
+             const user = await database.Tokens.get(req.query.token); 
+             const post = await database.Posts.get(req.query.PostID); 
+  
+             if (user) { 
+                 if (post) {}
+                 else return res.json({ 
+                   error: "The provided post id is invalid.", 
+                 }); 
+             } else 
+                return res.json({ 
+                  error: "The provided user token is invalid, or the user does not exist.", 
+                });
          }
 };
