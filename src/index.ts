1262220a41719abe26992a99387b2b7cfe0f18f6
selectdev <selectdev01@gmail.com>
2023-12-23 14:23:36 +0000

    fastify ig idk

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 // Packages
-import express, { Express, Request, Response } from "express";
 import fs from "fs";
-import cookieParser from "cookie-parser";
 import firebase from "firebase-admin";
 import serviceAccount from "./firebaseService.js";
 import path from "path";
 import * as database from "./database/handler.js";
 import * as logger from "./logger.js";
-import cors from "cors";
 import "dotenv/config";
-import { DecodedIdToken } from "firebase-admin/auth";
+import Fastify, {
+	FastifyInstance,
+	RouteOptions,
+} from "fastify";
+import fastifyCors from "fastify-cors";
 
 // Initialize Firebase Admin
 const firebaseService = firebase.initializeApp({
@@ -19,14 +20,17 @@ const firebaseService = firebase.initializeApp({
 });
 
 // Middleware
-const app: Express = express();
-app.use(cookieParser());
-app.use(cors());
-app.use(express.json());
+const app: FastifyInstance = Fastify({
+	logger: true,
+});
+
+app.register(fastifyCors, {
+	origin: true,
+});
 
 // API Endpoints Map
-const getFilesInDirectory = (dir) => {
-	let files = [];
+const getFilesInDirectory = (dir: string) => {
+	let files: string[] = [];
 	const filesInDir = fs.readdirSync(dir);
 
 	for (const file of filesInDir) {
@@ -41,7 +45,118 @@ const getFilesInDirectory = (dir) => {
 	return files;
 };
 
-const apiEndpoints = new Map();
+// API Endpoints
+let Routes: RouteOptions[] = [
+	{
+		method: ["GET", "POST", "PATCH", "HEAD", "OPTIONS", "DELETE"],
+		url: "/auth/signup",
+		schema: {
+			querystring: {
+				type: "object",
+				properties: {
+					tag: { type: "string" },
+					uid: { type: "string" },
+					token: { type: "string" },
+				},
+				required: ["tag", "uid", "token"],
+			},
+		},
+		handler: async (request, reply) => {
+			try {
+				const { token, tag }: any = request.params;
+
+				const userInfo = await firebaseService
+					.auth()
+					.verifyIdToken(token, true);
+				const dbUser = await database.Users.get({
+					userid: userInfo.uid,
+				});
+
+				if (dbUser) {
+					return reply.send({
+						error: true,
+						message: "[Database Error] => User already exists.",
+					});
+				} else {
+					const existingUser = await database.Users.get({
+						usertag: tag,
+					});
+					if (existingUser) {
+						return reply.send({
+							error: true,
+							message:
+								"That username is already in use. Please choose a new one.",
+						});
+					} else {
+						const result = await database.Users.createUser(
+							tag,
+							userInfo.uid,
+							tag,
+							"None",
+							"/logo.png"
+						);
+
+						if (result === true)
+							return reply.send({
+								error: false,
+								message: "User Created.",
+							});
+						else
+							return reply.send({ error: true, message: result });
+					}
+				}
+			} catch (error) {
+				reply.status(500).send({
+					error: "Internal Server Error",
+					message: "An error occurred while processing your request.",
+				});
+
+				logger.error("Error during user signup", error);
+			}
+		},
+	},
+	{
+		method: ["GET", "POST", "PATCH", "HEAD", "OPTIONS", "DELETE"],
+		url: "/auth/callback",
+		schema: {
+			querystring: {
+				type: "object",
+				properties: {
+					token: { type: "string" },
+				},
+				required: ["token"],
+			},
+		},
+		handler: async (request, reply) => {
+			try {
+				const { token }: any = request.params;
+
+				const userInfo = await firebaseService
+					.auth()
+					.verifyIdToken(token, true);
+				const dbUser = await database.Users.get({
+					userid: userInfo.uid,
+				});
+
+				if (dbUser) return reply.send({ token: token });
+				else
+					return reply.send({
+						token: token,
+						error: true,
+						message: "User does not exist.",
+					});
+			} catch (error) {
+				reply.status(500).send({
+					error: "Internal Server Error",
+					message: "An error occurred while processing your request.",
+				});
+
+				logger.error("Error during authentication callback", error);
+			}
+		},
+	},
+];
+
 const apiEndpointsFiles = getFilesInDirectory("./dist/endpoints").filter(
 	(file) => file.endsWith(".js")
 );
@@ -50,126 +165,21 @@ for (const file of apiEndpointsFiles) {
 	import(`../${file}`)
 		.then((module) => {
 			const endpoint = module.default;
-			apiEndpoints.set(
-				`${endpoint.name}:${endpoint.method.toLowerCase()}`,
-				endpoint
-			);
+			Routes.push(endpoint);
 		})
 		.catch((error) => {
 			console.error(`Error importing ${file}: ${error}`);
 		});
 }
 
-// API Endpoints
-app.all(`/api/:category/:endpoint`, async (req: Request, res: Response) => {
-	const endpoint = `${req.params.category}/${
-		req.params.endpoint
-	}:${req.method.toLowerCase()}`;
-	const data = apiEndpoints.get(endpoint);
-
-	if (data) {
-		if (data.method != req.method)
-			return res.status(405).json({
-				error: `Method "${data.method}" is not allowed for endpoint "${endpoint}"`,
-			});
-
-		try {
-			await data.execute(req, res, database, firebase);
-		} catch (error) {
-			res.status(500).json({
-				error: "Internal Server Error",
-				message: "An error occurred while processing your request.",
-			});
-
-			logger.error(`API (${endpoint})`, error);
-		}
-	} else
-		return res.status(404).json({
-			error: "This endpoint does not exist.",
-		});
-});
-
-const check = (query: any): boolean => {
-	if (!query || query === "") return true; // empty
-	else return false; // has content
-};
-
-app.all("/auth/signup", async (req: Request, res: Response) => {
-	if (check(req.query.tag))
-		return res.json({
-			error: "Missing query: tag",
-		});
-	if (check(req.query.uid))
-		return res.json({
-			error: "Missing query: uid",
-		});
-	if (check(req.query.token))
-		return res.json({
-			error: "Missing query: token",
-		});
-
-	const userInfo: DecodedIdToken = await firebaseService
-		.auth()
-		.verifyIdToken(req.query.token as string, true);
-
-	const dbUser = await database.Users.get({ userid: userInfo.uid });
-
-	if (dbUser)
-		return res.json({
-			error: true,
-			message: "[Database Error] => User already exists.",
-		});
-	else {
-		const dwp = await database.Users.get({
-			usertag: req.query.tag as string,
-		});
-
-		if (dwp)
-			return res.json({
-				error: true,
-				message:
-					"That username is already in use. Please choose a new one.",
-			});
-		else {
-			const i = await database.Users.createUser(
-				req.query.tag as string,
-				userInfo.uid,
-				req.query.tag as string,
-				"None",
-				"/logo.png"
-			);
-
-			if (i === true)
-				return res.json({ error: false, message: "User Created." });
-			else return res.json({ error: true, message: i });
-		}
-	}
-});
-
-app.all("/auth/callback", async (req: Request, res: Response) => {
-	if (check(req.query.token))
-		return res.json({
-			error: true,
-			message:
-				"There was no Authentication Token specified with this request.",
-		});
-
-	const userInfo: DecodedIdToken = await firebaseService
-		.auth()
-		.verifyIdToken(req.query.token as string, true);
-
-	const dbUser = await database.Users.get({ userid: userInfo.uid });
-
-	if (dbUser) return res.json({ token: req.query.token });
-	else
-		return res.json({
-			token: req.query.token,
-			error: true,
-			message: "User does not exist.",
-		});
-});
+setTimeout(() => {
+	if (Routes.length === apiEndpointsFiles.length + 2)
+		Routes.forEach((route) => app.route(route));
+}, 2000);
 
 // Start Server
-app.listen(process.env.PORT, async () => {
-	logger.success("Server", `Hosting web server on port ${process.env.PORT}.`);
-});
+try {
+	app.listen(process.env.PORT);
+} catch (err) {
+	throw new Error(err);
+}
