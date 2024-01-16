5555957fc315d02c57f9966ddbc90f734761a947
selectdev <selectdev01@gmail.com>
2024-01-16 12:36:14 +0000

    Migration Script almost done

--- src/migrate.ts
+++ src/migrate.ts
@@ -2,65 +2,28 @@ import * as v1 from "./v1-database/handler.js";
 import { prisma } from "./v2-database/prisma.js";
 import * as logger from "./logger.js";
 
-(async () => {
+logger.info("Database V1", "Initializing...");
+logger.info("Database V2", "Initializing...");
+logger.info("Database Migration", "Getting Started...");
+
+// Nuke all of the new database tables
+Promise.all([
+	prisma.plugins.deleteMany({}),
+	prisma.commentPlugins.deleteMany({}),
+	prisma.comments.deleteMany({}),
+	prisma.posts.deleteMany({}),
+	prisma.applications.deleteMany({}),
+	prisma.users.deleteMany({}),
+]);
+
+setTimeout(async () => {
+	// Fetch all data from old database.
 	const oldApps = await v1.Applications.findAll();
 	const oldOnlyfoodzPosts = await v1.OnlyfoodzPosts.findAll();
 	const oldPosts = await v1.Posts.findAll();
 	const oldUsers = await v1.Users.findAll();
 
-	oldApps.forEach(async (oldApp) => {
-		await prisma.applications.create({
-			data: {
-				creatorid: oldApp.creatorid,
-				name: oldApp.name,
-				logo: oldApp.logo,
-				token: oldApp.token,
-				active: oldApp.active,
-				permissions: oldApp.permissions,
-			},
-		});
-
-		logger.success("Apps", `Migrated App: ${oldApp.name}`);
-	});
-
-	oldPosts.forEach(async (oldPost) => {
-		await prisma.posts.create({
-			data: {
-				userid: oldPost.userid,
-				caption: oldPost.caption,
-				type: 0,
-				image: oldPost.image,
-				postid: oldPost.postid,
-				upvotes: oldPost.upvotes,
-				downvotes: oldPost.downvotes,
-			},
-		});
-
-		logger.success(
-			"Posts",
-			`Migrated Sparkyflight Post: ${oldPost.postid}`
-		);
-	});
-
-	oldOnlyfoodzPosts.forEach(async (oldOnlyfoodzPost) => {
-		await prisma.posts.create({
-			data: {
-				userid: oldOnlyfoodzPost.userid,
-				caption: oldOnlyfoodzPost.caption,
-				type: 1,
-				image: oldOnlyfoodzPost.image,
-				postid: oldOnlyfoodzPost.postid,
-				upvotes: oldOnlyfoodzPost.upvotes,
-				downvotes: oldOnlyfoodzPost.downvotes,
-			},
-		});
-
-		logger.success(
-			"Posts",
-			`Migrated Onlyfoodz Post: ${oldOnlyfoodzPost.postid}`
-		);
-	});
-
+	// Start Migration (Old => New)
 	oldUsers.forEach(async (oldUser) => {
 		await prisma.users.create({
 			data: {
@@ -78,4 +41,59 @@ import * as logger from "./logger.js";
 
 		logger.success("Users", `Migrated User: ${oldUser.userid}`);
 	});
-})();
+
+	setTimeout(async () => {
+		oldApps.forEach(async (oldApp) => {
+			await prisma.applications.create({
+				data: {
+					creatorid: oldApp.creatorid,
+					name: oldApp.name,
+					logo: oldApp.logo,
+					token: oldApp.token,
+					active: oldApp.active,
+					permissions: oldApp.permissions,
+				},
+			});
+
+			logger.success("Apps", `Migrated App: ${oldApp.name}`);
+		});
+
+		oldPosts.forEach(async (oldPost) => {
+			await prisma.posts.create({
+				data: {
+					userid: oldPost.userid,
+					caption: oldPost.caption,
+					type: 0,
+					image: oldPost.image,
+					postid: oldPost.postid,
+					upvotes: oldPost.upvotes,
+					downvotes: oldPost.downvotes,
+				},
+			});
+
+			logger.success(
+				"Posts",
+				`Migrated Sparkyflight Post: ${oldPost.postid}`
+			);
+		});
+
+		oldOnlyfoodzPosts.forEach(async (oldOnlyfoodzPost) => {
+			await prisma.posts.create({
+				data: {
+					userid: oldOnlyfoodzPost.userid,
+					caption: oldOnlyfoodzPost.caption,
+					type: 1,
+					image: oldOnlyfoodzPost.image,
+					postid: oldOnlyfoodzPost.postid,
+					upvotes: oldOnlyfoodzPost.upvotes,
+					downvotes: oldOnlyfoodzPost.downvotes,
+				},
+			});
+
+			logger.success(
+				"Posts",
+				`Migrated Onlyfoodz Post: ${oldOnlyfoodzPost.postid}`
+			);
+		});
+	}, 2000);
+}, 4000);
